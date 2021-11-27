3b91eb61e14c2023537084fedc0b4c4a37ad353c
ReyterYT <reyteryt5@gmail.com>
2021-11-27 21:14:38 +0700

    slight changes to run.py to make it able to run on uvloop, and added aiopg extra for economy system so asyncpg is only mandatory for articles management

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
 import click
+import logging
 from bot import get_bot
 
+logger = logging.getLogger("bot")
 
 @click.command()
 @click.option("--use-color", "--use-colour", "-c", type=bool, default=True)
-def main(use_color: bool):
+@click.option("-l", "--event-loop", type=click.Choice(["asyncio", "uvloop"], False), default="best")
+def main(use_color: bool, event_loop: str):
+    if event_loop.lower() == "uvloop":
+        logger.debug("Using 'uvloop'")
+        import uvloop # type: ignore
+        uvloop.install()
+    elif event_loop == "best":
+        try:
+            logger.debug("Trying 'uvloop'")
+            import uvloop # type: ignore
+            uvloop.install()
+        except ImportError:
+            logger.debug("'uvloop' failed (module not installed), falling back to 'asyncio'")
+
     bot, token = get_bot(use_colour=use_color)
     bot.run(token)
 
