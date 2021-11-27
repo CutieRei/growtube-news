b636639d699493f5ff1ee2ea936eb26e98b1913c
ReyterYT <reyteryt5@gmail.com>
2021-11-27 19:15:39 +0700

    reformatted info

--- ext/utils.py
+++ ext/utils.py
@@ -25,18 +25,19 @@ class Utility(commands.Cog):
         python_proc = psutil.Process(os.getpid())
         meminfo = python_proc.memory_info()
         uptime = datetime.utcnow() - self.bot.uptime
+        value = f"""```
+        Memory Usage: {meminfo.rss / (1024 ** 2):.2f}MiB
+        Virtual Memory Usage: {meminfo.vms / (1024 ** 2):.2f}MiB
+        Threads: {len(threading.enumerate())}
+        Child Processes: {len(multiprocessing.active_children())}
+        Tasks: {len(asyncio.all_tasks())}
+        Lib: {discord.__name__}=={discord.__version__} by {discord.__author__}
+        Uptime: {humanize.precisedelta(uptime)}
+        ```
+        """
         embed.add_field(
             name="Bot Usage",
-            value=f"""```
-            Memory Usage: {meminfo.rss / (1024 ** 2):.2f}MiB
-            Virtual Memory Usage: {meminfo.vms / (1024 ** 2):.2f}MiB
-            Threads: {len(threading.enumerate())}
-            Processes: {len(multiprocessing.active_children())}
-            Tasks: {len(asyncio.all_tasks())}
-            Lib: {discord.__name__}=={discord.__version__} by {discord.__author__}
-            Uptime: {humanize.precisedelta(uptime)}
-            ```
-            """,
+            value=value,
         )
         virtual_memory = psutil.virtual_memory()
         cpus_percent = "\n".join(
@@ -45,17 +46,18 @@ class Utility(commands.Cog):
                 for index, p in enumerate(psutil.cpu_percent(percpu=True))
             ]
         )
+        value = f"""```
+        Available Memory: {virtual_memory.available}
+        Used Memory: {virtual_memory.used}
+        Free Memory: {virtual_memory.free}
+        Cpu Usages: 
+            {cpus_percent}
+        Cpu Frequency (total): {psutil.cpu_freq()}
+        ```
+        """
         embed.add_field(
             name="System Info",
-            value=f"""```
-            Available Memory: {virtual_memory.available}
-            Used Memory: {virtual_memory.used}
-            Free Memory: {virtual_memory.free}
-            Cpu Usages: 
-                {cpus_percent}
-            Cpu Frequency (total): {psutil.cpu_freq()}
-            ```
-            """,
+            value=value,
         )
         await ctx.send(embed=embed)
 
